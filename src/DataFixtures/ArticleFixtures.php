8c76fd7afde0d1a32d534f8cdb42b92510ec28f9
filox09 <filox59@gmail.com>
2021-02-02 05:14:15 +0100

    2 entities create Category.php & Comment.php ArticleFixtures updated with PHPfaker and adding category and comments

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,25 +5,58 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article; /* ne pas oublier un user pour expliquer a php d'ou vien la class Article */
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++) { /* Création de 10 articles */ 
-            $article = new Article(); /* Création d'une nouvelle instence de la class Article() */
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Maecenas vel egestas justo. Nam ornare ornare libero nec aliquet. Pellentesque convallis, dui ac pharetra ullamcorper, diam lectus suscipit risus, nec lobortis nulla eros a odio. Donec tristique neque orci. Sed elit leo, dignissim quis dignissim nec, blandit eu urna. Phasellus in sollicitudin dolor. Nulla facilisi. Aliquam erat volutpat. Donec consectetur magna sed lectus facilisis, in elementum massa mollis. Ut euismod ut risus ullamcorper posuere. Mauris aliquet dignissim mollis. Maecenas hendrerit porta ipsum, ut eleifend augue.
-                                </p><p>Maecenas vel egestas justo. Nam ornare ornare libero nec aliquet. Pellentesque convallis, dui ac pharetra ullamcorper, diam lectus suscipit risus, nec lobortis nulla eros a odio. Donec tristique neque orci. Sed elit leo, dignissim quis dignissim nec, blandit eu urna. Phasellus in sollicitudin dolor. Nulla facilisi. Aliquam erat volutpat. Donec consectetur magna sed lectus facilisis, in elementum massa mollis. Ut euismod ut risus ullamcorper posuere. Mauris aliquet dignissim mollis. Maecenas hendrerit porta ipsum, ut eleifend augue.
-                                </p><p>Maecenas vel egestas justo. Nam ornare ornare libero nec aliquet. Pellentesque convallis, dui ac pharetra ullamcorper, diam lectus suscipit risus, nec lobortis nulla eros a odio. Donec tristique neque orci. Sed elit leo, dignissim quis dignissim nec, blandit eu urna. Phasellus in sollicitudin dolor. Nulla facilisi. Aliquam erat volutpat. Donec consectetur magna sed lectus facilisis, in elementum massa mollis. Ut euismod ut risus ullamcorper posuere. Mauris aliquet dignissim mollis. Maecenas hendrerit porta ipsum, ut eleifend augue.
-                                </p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        // use the factory to create a Faker\Generator instance
+        $faker = \Faker\Factory::create('fr_FR');
+        
+        //Créer 3 catégories fake
+        for($i = 1; $i <= 3; $i++ ) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+            
+            $manager->persist($category);
 
-            $manager->persist($article); /* Demander au manager de se préparrer a faire percisté mon article */
+            // Créer entre 4 et 6 articles
+            for($j = 1; $j <= mt_rand(4, 6); $j++) { /* Création de 10 articles */ 
+                $article = new Article(); /* Création d'une nouvelle instence de la class Article() */
+              
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+            
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
 
+                $manager->persist($article); /* Demander au manager de se préparrer a faire percisté mon article */
+
+                // on donne des commentaires à l'article
+                for($k = 1; $k <= mt_rand(4, 10); $k++) {
+
+                    $comment = new Comment();
+
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+                    
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
+                            ->setArticle($article);
+                    
+                    $manager->persist($comment);
+
+                }
+            }
         }
-
+        
         $manager->flush(); /* Pour envoyer la requête SQL qui va mettre en place les fausses données */
     }
 }
