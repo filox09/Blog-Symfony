8614ec04e7afbbcd9b36e40b47b812d09a3eaf1e
filox09 <filox59@gmail.com>
2021-02-03 02:46:38 +0100

    adding loop for comments in show.html.twig & adding a label for select category when creating or editing articles

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,6 +15,11 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title', null ,array('label'=>'Titre de l\'article'))
+            ->add('category', EntityType::class, array(
+                'class' => Category::class,
+                'choice_label' => 'title',
+                'label'=>'Choisir une catégorie'
+            ))
             ->add('content', null ,array('label'=>'Contenu de l\'article'))
             ->add('image', null ,array('label'=>'Image de l\'article', 'attr' => ['class' => 'test-de-class']))
         ;
