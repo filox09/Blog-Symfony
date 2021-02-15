09c688c0ab1180152140c6aacf6a3afaa14dd152
filox09 <filox59@gmail.com>
2021-02-15 18:48:30 +0100

    test deploy fix bug migrations

--- migrations/Version20210202011459.php
+++ migrations/Version20210202011459.php
@@ -22,8 +22,6 @@ final class Version20210202011459 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, article_id INT NOT NULL, author VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_9474526C7294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id)');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
-        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
     }
 
     public function down(Schema $schema) : void
